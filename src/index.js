a09a09a9029c639a0f4cd610ab9c90112bf4131e
Pytniza <ip.web.so@gmail.com>
2018-10-07 21:00:41 +0300

    pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  let counter = 1, max = 0;
+  if (array.length < 2) {return array.length}
+  if (array.length < 10) array.sort((a, b) => a - b)
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] + 1 == array[i+1]) {counter++}
+    if (array[i] == array[i+1]) continue;
+    if (counter > max) {max = counter}
+    if (array[i] + 1 != array[i+1]) {counter = 1}
+  }
+return max;
 }
